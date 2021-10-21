a8a1613c330302bee9f96be61bc3a26fa7411be8
José R. Correas <correas@cop.es>
2021-10-21 12:14:36 +0200

    go files

---
+++ startservermode.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net"
+)
+
+func main() {
+
+    con, err := net.Dial("tcp", "webcode.me:80")
+    checkError(err)
+
+    req := "GET / HTTP/1.0\r\n" +
+        "Host: webcode.me\r\n" +
+        "User-Agent: Go client\r\n\r\n"
+
+    _, err = con.Write([]byte(req))
+    checkError(err)
+
+    res, err := ioutil.ReadAll(con)
+    checkError(err)
+
+    fmt.Println(string(res))
+}
+
+func checkError(err error) {
+
+    if err != nil {
+        log.Fatal(err)
+    }
+}
